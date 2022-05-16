3d8d8d1fad784a3eb2ed881353f3c6dd87306e04
unknown <andy78644.cs10g@o365.nctu.edu.tw>
2022-05-17 00:14:46 +0800

    buy one 1 and two 2 for test in green light

--- apps/potter/src/app/game.ts
+++ apps/potter/src/app/game.ts
@@ -1,16 +1,35 @@
 export class Game {
     private total_price = 0;
-    private book = 0
+    private book = 0;
+    private total_book = 0;
+    private count: number [] = [0, 0, 0, 0, 0, 0];
     buy(pins: number){
-        this.total_price += 100;
-        this.book++;
+        //this.total_price += 100;
+        this.count[pins]++;
+        this.total_book++;
     }
-    
+    private test = 2;
     get price(){
-        if(this.book === 2) this.total_price *= 0.95;
-        else if(this.book === 3) this.total_price *= 0.90;
-        else if(this.book === 4) this.total_price *= 0.80;
-        else if(this.book === 5) this.total_price *= 0.75;
+        while(this.total_book>0){
+            this.book = 0;
+            for(let index = 1; index <6;index++){
+                if(this.count[index]>0){
+                    this.book++;
+                    this.count[index]--;
+                    this.total_book--;
+                }
+            }
+            this.test--;
+
+            //this.total_book--;
+            if(this.book === 1) this.total_price += 100;
+            else if(this.book === 2) this.total_price += 190;
+            else if(this.book === 3) this.total_price += 270;
+            else if(this.book === 4) this.total_price += 320;
+            else if(this.book === 5) this.total_price += 375;
+            
+        }
+        //return this.total_book;
         return this.total_price;
     }
 }
